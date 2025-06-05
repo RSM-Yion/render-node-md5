580fd885766941abfb9cefb2d8a03ed99552fd07
Yion <fengyion@rsm.com.sg>
2025-06-05 11:47:13 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+app.use(express.json());
+
+app.post('/sign', (req, res) => {
+  const {
+    method, uri, developerId, appId,
+    appSecret, requestId, time
+  } = req.body;
+
+  const signString =
+    `method=${method}&uri=${uri}&developerId=${developerId}` +
+    `&appId=${appId}&appSecret=${appSecret}` +
+    `&requestId=${requestId}&time=${time}`;
+
+  const signature = crypto
+    .createHash('md5')
+    .update(signString, 'utf8')
+    .digest('hex')
+    .toUpperCase();
+
+  res.json({ signString, signature });
+});
+
+app.listen(3000, () => {
+  console.log('✅ MD5 service running at http://localhost:3000/sign');
+});
+/*
+const crypto = require("crypto");
+
+const str = "method=GET&uri=/api/v1/cits/access/1081&developerId=2VE5aerqWs1P8nVZB9ECHB&appId=3dlRhfncSBpzAy8Bzo4Mlt&appSecret=72dJovbQ4i8rClgcLhtvin&requestId=111&time=1749028328419";
+
+const signature = crypto
+  .createHash("md5")
+  .update(str, "utf8")
+  .digest("hex")
+  .toUpperCase();
+
+console.log("Expected Signature:", signature);
+*/
